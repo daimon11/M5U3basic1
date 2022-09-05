f2be64af19c704bd606f509add8e877902160a1a
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-09-05 12:42:21 +0300

    add asking

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 'use strict';
 
 const start = () => {
-  const list = document.querySelector('.list');
-  const userDate = prompt('Введите данные:', '');
-  const userElem = document.createElement('li');
-  console.log(userDate);
-  const exit = () => {
-    const choice = confirm('Точно ли Вы хотите выйти?');
-    if (choice === true) {
-      alert(`Пока-пока!`);
-      return;
-    } else {
-      return start();
-    }
+  const deleteValue = (a) => {
+    a.removeChild(a.lastElementChild);
+    return start();
   };
 
-  const dataVerification = (a) => {
+  const clearValue = (a) => {
+    a.textContent = '';
+    return start();
+  };
+
+  const writeValue = (a, b, c) => {
+    b.insertAdjacentElement('beforeend', c);
+    c.insertAdjacentText('beforeend', a);
+    return start();
+  };
+
+  const asking = () => {
+    const list = document.querySelector('.list');
+    const userElem = document.createElement('li');
+    const userDate = prompt('Введите данные:', '');
     switch (true) {
-      case (a === 'del'):
-        list.removeChild(list.lastElementChild);
-        return start();
-      case (a === 'clear'):
-        list.textContent = '';
-        return start();
-      case (a === 'exit' || a === null):
-        return exit();
+      case (userDate === 'del'):
+        deleteValue(list);
+        break;
+      case (userDate === 'clear'):
+        clearValue();
+        break;
+      case (userDate === 'exit' || userDate === null):
+        break;
+      case (userDate.trim() !== ''):
+        writeValue(userDate, list, userElem);
+        break;
       default:
-        return a;
-    }
-  };
-  const writeValue = (b, c, d) => {
-    if (dataVerification(b) === undefined) {
-      return;
-    } else {
-      c.insertAdjacentElement('beforeend', d);
-      d.insertAdjacentText('beforeend', b);
-      return start();
+        return asking();
     }
   };
 
-  return writeValue(userDate, list, userElem);
+  return asking();
 };
 start();
